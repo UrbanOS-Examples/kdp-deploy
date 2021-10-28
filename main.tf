f4796200d7a8b4724798481e7deb4b74383f4b28
Timothy Regan <tregan@pillartechnology.com>
2021-10-28 13:51:18 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -186,7 +186,7 @@ export KUBECONFIG=${local_file.kubeconfig.filename}
 export AWS_DEFAULT_REGION=us-east-2
 
 (
-helm repo add scdp https://urbanos-public.github.io/
+helm repo add scdp https://urbanos-public.github.io/charts
 helm repo update
 helm dependency update
 helm upgrade --install kdp \
